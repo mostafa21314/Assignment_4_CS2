948abb79583b1b578c9343c57ac45190e7c3cbe8
mostafa21314 <mostafa21314@aucegypt.edu>
2023-04-20 16:52:18 +0200

    first commit

--- Assignment_4_CS2/LinkedList.cpp
+++ Assignment_4_CS2/LinkedList.cpp
@@ -1,6 +1,51 @@
 #include "LinkedList.h"
 #include<iostream>
 using namespace std;
+
+
+LinkedList::LinkedList()
+{
+    head = NULL;
+}
+LinkedList::~LinkedList()
+{
+    Node* temp = head;
+    Node* temp2 = temp;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        delete temp2;
+        temp2 = temp;
+    }
+}
+void LinkedList::addNode(int x)
+{
+    if (head == NULL)
+    {
+        head = new Node;
+        head->value = x;
+        head->occur = 1;
+    }
+    else
+    {
+        int occ = 1;
+        Node* temp = head;
+        while (temp != NULL)
+        {
+            if (temp->value == x)
+            {
+                occ++;
+            }
+            if (temp->next == NULL)
+            {
+                temp->next = new Node;
+                temp->next->value = x;
+                temp->next->occur = occ;
+            }
+            temp = temp->next;
+        }
+    }
+}
 void LinkedList::PrintList() const
 {
     Node* temp = head;
@@ -10,4 +55,15 @@ void LinkedList::PrintList() const
         temp = temp->next;
     }
     cout << endl;
+}
+int LinkedList::sumList() const
+{
+    int sum = 0;
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        sum += temp->value;
+        temp = temp->next;
+    }
+    return sum;
 }
